4368d4c3e10a1301e48e7c9e5337ec517f518945
unknown <parfenova-viktoria@yandex.ru>
2024-05-09 22:59:44 +0300

    added code

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+System.Console.WriteLine("Введите элементы массива через пробел:");
+string[] InputArray = Console.ReadLine().Split(' ');
+
+int count = 0;
+    foreach (string str in InputArray)
+    {
+        if (str.Length <= 3)
+        {
+            count++;
+        }
+    }
+
+string [] OutputArray = new string[count];
+Console.WriteLine("Новый массив:");
+
+int index = 0;
+    foreach (string str in InputArray)
+    {
+        if (str.Length <= 3)
+        {
+            OutputArray[index] = str;
+            System.Console.Write(OutputArray[index] + " ");
+            index++;
+        }
+    }
